a7cf741dbb0ef161230cfa589b64ddacb3806d5d
Dilschat <dilschat@yandex.ru>
2018-08-06 11:22:09 +0300

    phenopackets building deleted

--- build_external_libs.sh
+++ build_external_libs.sh
@@ -9,18 +9,6 @@ curl -LJO -o cramtools-3.0.jar  https://github.com/enasequence/cramtools/raw/mas
 # Install cramtools locally
 mvn install:install-file -Dfile=cramtools-3.0.jar -DgroupId=com.github.cramtools -DartifactId=cramtools  -Dversion=3.0 -Dpackaging=jar
 
-echo "Building and installing phenopackets-schema"
-#building and installing phenopackets-schema
-cd phenopacket-schema
-
-#get specific commit fot avoiding errors before release
-git reset --hard c67527e48f94fa71e8763c3a2a3234917b5c5bcb
-./mvn clean install
-cd ..
-
-# we have already installed the package, do we need to reinstall it?
-mvn install:install-file -Dfile=./phenopacket-schema/target/phenopacket-schema-0.0.7.jar -DgroupId=org.phenopackets.schema.v1 -DartifactId=phenopacket-schema -Dversion=0.0.7-SNAPSHOT -Dpackaging=jar
-
 echo "Building and installing picard"
 #building picard
 git clone https://github.com/broadinstitute/picard.git
